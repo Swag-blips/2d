01043be65176910aa6d0701959a97315ca0e1214
Swag-blips <coderblip@gmail.com>
2024-11-07 11:52:25 +0100

    feat:game-complete

--- main.js
+++ main.js
@@ -37,6 +37,8 @@ let cursors;
 let score = 0;
 let scoreText;
 let stars;
+let bombs;
+let gameOver = false;
 function create() {
   this.add.image(400, 300, "sky");
 
@@ -93,6 +95,12 @@ function create() {
     fontSize: "32px",
     fill: "#000",
   });
+
+  bombs = this.physics.add.group();
+
+  this.physics.add.collider(bombs, platforms);
+
+  this.physics.add.collider(player, bombs, hitBomb, null, this);
 }
 
 function collectStar(player, star) {
@@ -100,6 +108,30 @@ function collectStar(player, star) {
 
   score += 10;
   scoreText.setText("Score:" + score);
+
+  if (stars.countActive(true) === 0) {
+    stars.children.iterate(function (child) {
+      child.enableBody(true, child.x, 0, true, true);
+    });
+
+    let x =
+      player.x < 400
+        ? Phaser.Math.Between(400, 800)
+        : Phaser.Math.Between(0, 400);
+
+    let bomb = bombs.create(x, 16, "bomb");
+    bomb.setBounce(1);
+    bomb.setCollideWorldBounds(true);
+    bomb.setVelocity(Phaser.Math.Between(-200, 200), 20);
+  }
+}
+
+function hitBomb(player, bomb) {
+  this.physics.pause();
+  player.setTint(0xff0000);
+  player.anims.play("turn");
+
+  gameOver = true;
 }
 function update() {
   if (cursors.left.isDown) {
